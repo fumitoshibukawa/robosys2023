549d54854c1679313534ccd9f43195c859687350
Fumito Shibukawa <spaceyoungsoldier69@gmail.com>
2023-06-26 13:01:24 +0900

    Add tests

--- test.bash
+++ test.bash
@@ -1,9 +1,9 @@
-#!/bin/bash -xv 
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2023 Fumito Shibukawa
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG at Line$1
+	echo NG at Line $1
 	res=1
 }
 
@@ -12,6 +12,15 @@ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng $LINENO
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res 
